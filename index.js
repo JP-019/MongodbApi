bbb76a85206e3c781fcf21d900412655c34966da
. <juliocgp30@gmail.com>
2026-05-17 21:33:11 -0600

    Agregando Cors para servicios compartidos

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const redoc = require("redoc-express");
 const app = express();
 require("dotenv").config();
 
+app.use(cors());
 app.use(express.json());
 
 const connectDB = require("./src/config/database");
